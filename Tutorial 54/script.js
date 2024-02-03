994b95351d8f464593fca45635f6f1fcad3cdf35
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-02-04 00:02:03 +0530

    Introduction to JavaScript

---
+++ Tutorial 54/script.js
@@ -0,0 +1,16 @@
+alert("Hello Akash");
+
+console.log("Code is Running .....");
+console.log("Code is Running .....");
+console.log("Code is Running .....");
+
+var a = prompt("Enter Your Number :");
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer");
+
+if(isTrue){
+    console.log("Computer is Blasting !");
+} else {
+    console.log("Computer is not Blasting !");
+}
+
+console.log("Your Number is " + a);
