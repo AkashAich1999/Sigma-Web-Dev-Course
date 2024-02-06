bcd32b909277ec8a06751b7e0e2419a417db94c3
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-02-07 03:16:34 +0530

    Faulty Calculator Solution (Second Way)

--- Tutorial 61/script.js
+++ Tutorial 61/script.js
@@ -13,25 +13,55 @@ This faulty calculator does following:
 
 */
 
+
+// METHOD 1 (One Way) :
+
+// let random = Math.random();
+// console.log(random);
+// let a = prompt("Enter First Number");
+// let c = prompt("Enter Operation");
+// let b = prompt("Enter Second Number");
+
+// let obj = {
+//     "+" : "-",
+//     "*" : "+",
+//     "-" : "/",
+//     "/" : "**",
+// }
+
+// if(random > 0.1) {
+//     // Perform Correcct Calculation
+//     alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+
+// } else {
+//    // Perform Wrong Calculation
+//    c = obj[c];
+//    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+// }
+
+
+// METHOD 2 (Second Way) :
+
+console.log("This is a Calculator");
+
+// Enter First Number
+let x = prompt("Enter First Number");
+// Enter Second Number
+let y = prompt("Enter Second Number");
+
 let random = Math.random();
 console.log(random);
-let a = prompt("Enter First Number");
-let c = prompt("Enter Operation");
-let b = prompt("Enter Second Number");
 
-let obj = {
-    "+" : "-",
-    "*" : "+",
-    "-" : "/",
-    "/" : "**",
-}
-
-if(random > 0.1) {
+if(random < 0.1){
     // Perform Correcct Calculation
-    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
-
+    console.log("Addition is : ", x + y);
+    console.log("Substraction is : ", x - y);
+    console.log("Multiplication is : ", x * y);
+    console.log("Division is : ", x / y);
 } else {
-   // Perform Wrong Calculation
-   c = obj[c];
-   alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+    // Perform Wrong Calculation
+    console.log("Addition is : ", x - y);
+    console.log("Substraction is : ", x / y);
+    console.log("Multiplication is : ", x + y);
+    console.log("Division is : ", x ** y);
 }
