3586e6c353c2b8f9da876f983a243cfdfd8d8306
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-02-14 06:44:08 +0530

    Events, Event Bubbling, setInterval & setTimeout

---
+++ Tutorial 74/script.js
@@ -0,0 +1,43 @@
+let button = document.getElementById("btn");
+
+// button.addEventListener("click", () => {
+//     alert("I was clicked. Yayy !!");
+// });
+
+// button.addEventListener("click", () => {
+//     document.querySelector(".box").innerHTML = "<b>Button Clicked !!</b>";
+// });
+
+// button.addEventListener("dblclick", () => {
+//     document.querySelector(".box").innerHTML = "<b>Button Clicked !!</b>";
+// });
+
+// button.addEventListener("contextmenu", () => {
+//     alert("Right Mouse was Clicked !!");
+// });
+
+// button.addEventListener("keydown", (e) => {
+//     console.log(e);
+// });
+
+// document.addEventListener("keydown", (e) => {
+//     console.log(e);
+// });
+
+document.addEventListener("keydown", (e) => {
+    console.log(e, e.key, e.keyCode);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// List of all mouse events 
+// https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
